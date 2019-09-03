1475df20113d0d6dacfbb33efea5f8b27dc7b803
Fabian Peter Hammerle <fabianpeter.hammerle@catalysts.cc>
2019-09-03 16:14:22 +0200

    setup.py: added metadata for pypi.org; readme markdown->rest

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## unreleased
+
+### Added
+- Prepare push to pypi.org
+  - Converted readme from markdown to ReSructuredText format
+  - Added module metadata (author, maintainer, url)
+
 ## [1.0.0] 2019-09-03
 
 ### Added
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 import setuptools
 
+with open('README.rst', 'r') as readme:
+    LONG_DESCRIPTION = readme.read()
+
 setuptools.setup(
     name='dlinfo',
     use_scm_version=True,
+    author='Fabian Peter Hammerle',
+    author_email='fabianpeter.hammerle@catalysts.cc',
+    maintainer='Catalysts Space',
+    maintainer_email='space@catalysts.cc',
+    description='Python wrapper for libc\'s dlinfo',
+    long_description=LONG_DESCRIPTION,
+    url='https://code.grasp-open.com/grasp-tools/python-dlinfo',
     packages=setuptools.find_packages(),
     setup_requires=[
         'setuptools_scm',
